fd9ac0a635aade7f1b890283cd815e8b51f9912c
mc-greq <greq19@gmail.com>
2019-01-27 18:19:26 +0100

    JavaFX app with jdbc connection

--- src/sample/model/Datasource.java
+++ src/sample/model/Datasource.java
@@ -9,7 +9,7 @@ public class Datasource {
 
     public static final String DB_NAME = "music.db";
 
-    public static final String CONNECTION_STRING = "jdbc:sqlite:G:\\java\\intellij\\Music\\" + DB_NAME;
+    public static final String CONNECTION_STRING = "jdbc:sqlite:G:\\java\\intellij\\musicUi\\" + DB_NAME;
     public static final String TABLE_ALBUMS = "albums";
     public static final String COLUMN_ALBUM_ID = "_id";
     public static final String COLUMN_ALBUM_NAME = "name";
